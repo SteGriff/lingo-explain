37476b9527aa636e0fdcf6c626ead9f69fb62971
Ste Griffiths <github@stegriff.co.uk>
2025-02-24 15:39:49 +0000

    Refactor module

--- index.mjs
+++ index.mjs
@@ -1,35 +1,23 @@
-import OpenAI from 'openai';
-import { responseFormat } from './responseFormat.mjs';
-
-const openai = new OpenAI(); // key defaults to process.env["OPENAI_API_KEY"]
+import { explain } from "./explain.mjs";
 
 async function main() {
 
     // Ask GPT to explain a sentence from the target language
     // The output will be displayed as a number of bullet points
     // Each term-definition pair should be a JSON array member in the GPT response
-    // Build a schema to get this response using zod
 
     const targetLanguage = "Vietnamese";
-    const devPrompt = `Explain the ${targetLanguage} sentence word-by-word`;
-    const userPrompt = "Thái độ quan trọng hơn trình độ";
-    const userPrompt2 = "Tôi sẽ cố gắng viết thư cho cô ấy vào thứ năm tuần tới";
+    const testPrompts = [
+        "Thái độ quan trọng hơn trình độ",
+        "Tôi sẽ cố gắng viết thư cho cô ấy vào thứ năm tuần tới"
+    ];
 
-    const completion = await openai.chat.completions.create({
-        messages: [
-            { role: 'developer', content: devPrompt },
-            { role: 'user', content: userPrompt2 }
-        ],
-        max_tokens: 200,
-        model: 'gpt-4o-mini',
-        response_format: responseFormat
-    });
-
-    console.log(completion.choices);
-    const answerJson = completion.choices[0].message.content;
-    const answer = JSON.parse(answerJson);
-    for (const explanation of answer.explanation) {
-        console.log(`${explanation.term}: ${explanation.definition}`);
+    for (const prompt of testPrompts) {
+        console.log(`Explain: ${prompt}`);
+        const explanation = await explain(targetLanguage, prompt);
+        for (const word of explanation) {
+            console.log(`${word.term}: ${word.definition}`);
+        }
     }
 }
 
